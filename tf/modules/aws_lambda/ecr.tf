91ee7806454803b1d2dbae970f44494506a9f892
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2025-07-10 13:33:52 -0500

    Better cleanup lamnda function
    Retaining important images!

--- tf/modules/aws_lambda/ecr.tf
+++ tf/modules/aws_lambda/ecr.tf
@@ -19,23 +19,5 @@ resource "aws_ecr_repository" "lambda_repository" {
   }
 }
 
-resource "aws_ecr_lifecycle_policy" "lambda_repository_policy" {
-  repository = aws_ecr_repository.lambda_repository.name
-
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep last 10 images"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 10
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-}
+# Note: Lifecycle policy removed - images are now managed by cleanup Lambda
+# This ensures precise control over which images are deleted and when
