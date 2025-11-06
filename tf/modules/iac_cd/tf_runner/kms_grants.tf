de26a4ff220fdbb8d1dabbd79edf89b7285f2d36
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2025-11-06 10:04:40 -0500

    Fixing kms grants

--- tf/modules/iac_cd/tf_runner/kms_grants.tf
+++ tf/modules/iac_cd/tf_runner/kms_grants.tf
@@ -1,15 +1,10 @@
 # KMS Grants for cross-account CodeBuild access to pipeline artifacts
 # This allows the CodeBuild roles in this account to decrypt the KMS key in the tools account
 
-# Look up the KMS key ARN from parameter store (set by coreinfra in tools account)
-data "aws_ssm_parameter" "pipeline_artifacts_kms_key_arn" {
-  name = "/coreinfra/shared/pipeline_artifacts_kms_key_arn"
-}
-
 # Grant for terraform-plan CodeBuild role
 resource "aws_kms_grant" "codebuild_plan" {
   name              = "${var.org}-${var.app}-${var.env}-codebuild-terraform-plan"
-  key_id            = data.aws_ssm_parameter.pipeline_artifacts_kms_key_arn.value
+  key_id            = local.conventional_pipeline_kms_key_alias_arn
   grantee_principal = aws_iam_role.codebuild_terraform_plan.arn
   
   operations = [
@@ -26,7 +21,7 @@ resource "aws_kms_grant" "codebuild_plan" {
 # Grant for terraform-apply CodeBuild role
 resource "aws_kms_grant" "codebuild_apply" {
   name              = "${var.org}-${var.app}-${var.env}-codebuild-terraform-apply"
-  key_id            = data.aws_ssm_parameter.pipeline_artifacts_kms_key_arn.value
+  key_id            = local.conventional_pipeline_kms_key_alias_arn
   grantee_principal = aws_iam_role.codebuild_terraform_apply.arn
   
   operations = [
@@ -40,21 +35,5 @@ resource "aws_kms_grant" "codebuild_apply" {
   retiring_principal = aws_iam_role.codebuild_terraform_apply.arn
 }
 
-# Grant for terraform-apply-auto CodeBuild role (if enabled)
-resource "aws_kms_grant" "codebuild_apply_auto" {
-  count = var.enable_auto_apply ? 1 : 0
-
-  name              = "${var.org}-${var.app}-${var.env}-codebuild-terraform-apply-auto"
-  key_id            = data.aws_ssm_parameter.pipeline_artifacts_kms_key_arn.value
-  grantee_principal = aws_iam_role.codebuild_terraform_apply_auto[0].arn
-  
-  operations = [
-    "Decrypt",
-    "Encrypt",
-    "DescribeKey",
-    "GenerateDataKey"
-  ]
-  
-  # Allow the role to retire its own grant (cleanup)
-  retiring_principal = aws_iam_role.codebuild_terraform_apply_auto[0].arn
-}
+# Note: No separate grant needed for terraform-apply-auto
+# It reuses the same IAM role as terraform-apply (see codebuild_terraform_apply_auto.tf:7)
