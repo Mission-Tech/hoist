4decc57cd4461e9f40350d5fcc40371b8413a954
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2025-11-12 09:43:46 -0500

    Adding tags pattern
    Even though there's nothing to tag here yet

--- tf/modules/vercel_frontend/variables.tf
+++ tf/modules/vercel_frontend/variables.tf
@@ -1,3 +1,7 @@
+# ------------------------------------
+# -----------  Common inputs -----------
+# ------------------------------------
+
 variable "app" {
   description = "Name of the application"
   type        = string
@@ -7,6 +11,34 @@ variable "env" {
   description = "Name of the environment (dev or prod)"
   type        = string
 }
+
+variable "org" {
+  description = "The name of your organization (e.g., missiontech)"
+  type        = string
+}
+
+variable "repo" {
+  description = "The URL of the github repo managing this infrastructure"
+  type        = string
+}
+
+variable tags {
+  description = "Tags to apply to every resource"
+  type = map(string)
+}
+
+locals {
+  tags = merge(var.tags, {
+    app: var.app
+    env: var.env
+    org: var.org
+    repo: var.repo
+  })
+}
+
+# -------------------------------------------------
+# ----------- Module-specific variables -----------
+# -------------------------------------------------
 
 variable "public_app_name" {
   description = "Public-facing name for the app. Will appear as the subdomain for the custom domain (e.g., 'pantry' for pantry.missiontechdev.org)"
